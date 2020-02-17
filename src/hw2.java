364dcbe092d16a72dd2ec2edf0fd19e38a30c288
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2020-02-16 19:43:45 -0800

    readfile
    added readfile and files to read

--- src/hw2.java
+++ src/hw2.java
@@ -1,20 +1,53 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 
 public class hw2 {
-	ArrayList trainingData;
+	String[] names;
+	static ArrayList<String> attributes;
+	ArrayList<String> trainingData;
 	
-	
-	public void main (String[] Args) {
+
+	  public static void main(String[] args) {
+		  
+		//getData("src\\GRT.txt");
+		getData("src\\GR.txt");
+		getData("src\\keys.txt");
 		
-		readfile("GR.txt");
+		
+		
+	   
+	  }
+
+
+	private static void getData(String path) {
+		int x = 0;
+		 try {
+		      File myObj = new File("C:\\Users\\vanil\\eclipse-workspace\\LocalizationDM\\src\\GR.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        
+		        if(x<9) {
+		        for(String e: data.split(","))
+		        	{
+		        	attributes.add(e);
+		        	x++;
+		        	}
+		        }
+		        
+		        System.out.println(attributes);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+	}
 	}
 
-	private void readfile(String fName) {
-		File inputFile = new File(fName);
-		
-		
-	}
 
 
-}
