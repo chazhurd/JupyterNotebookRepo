8bf2de0bdaa4d236dbf555a8a03055018ad56d83
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2020-02-16 16:54:36 -0800

    Initial commit

---
+++ src/hw2.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class hw2 {
+	ArrayList trainingData;
+	
+	
+	public void main (String[] Args) {
+		
+		readfile("GR.txt");
+	}
+
+	private void readfile(String fName) {
+		File inputFile = new File(fName);
+		
+		
+	}
+
+
+}
